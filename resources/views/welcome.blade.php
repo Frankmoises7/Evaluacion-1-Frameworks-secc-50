3e01328b8fee7cb13a55822f6e41e6e633a35e89
daedov <daniqanil@gmail.com>
2022-07-24 16:02:41 -0400

    form changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,11 @@
 <form class="style-form">
     <h3 class="form-title mb-3">Ingresa a tu cuenta</h3>
     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <label for="exampleInputEmail1" class="form-label">Correo electrónico</label>
         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
         <input type="password" class="form-control" id="exampleInputPassword1">
     </div>
     <button type="submit" class="btn btn-light mt-3"><a href="{{url('/list')}}" class="a-link">Entrar</a></button>
