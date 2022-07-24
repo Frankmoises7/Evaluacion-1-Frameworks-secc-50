db8ee0b359e377508f88328c6012885670bf79c4
daedov <daniqanil@gmail.com>
2022-07-24 15:55:22 -0400

    adding more styles and connecting login with list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <form class="style-form">
-    <h3 class="mb-3">Ingresa a tu cuenta</h3>
+    <h3 class="form-title mb-3">Ingresa a tu cuenta</h3>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Correo electrónico</label>
         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -11,7 +11,7 @@
         <label for="exampleInputPassword1" class="form-label">Contraseña</label>
         <input type="password" class="form-control" id="exampleInputPassword1">
     </div>
-    <button type="submit" class="btn btn-light mt-3">Ingresar</button>
+    <button type="submit" class="btn btn-light mt-3"><a href="{{url('/list')}}" class="a-link">Entrar</a></button>
 </form>
 @stop
 
